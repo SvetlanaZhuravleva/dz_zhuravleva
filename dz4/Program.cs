116ed1a20c0515e49b84ae483fe2258fe1660c56
Svetlana <sigard_kerh@mail.ru>
2022-09-22 22:18:50 +0300

    fin com4

---
+++ dz4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число больше 0");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    int num1 = 0;
+    while (num >= num1)
+    {
+        Console.Write(num1 + ",");
+        num1 = num1 + 2;
+    }
+
+}
+else
+{
+    Console.WriteLine("Больше 0, умник");
+}
+
